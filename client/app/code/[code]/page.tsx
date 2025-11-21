a05890155b4d29acca5cd951fe7f2a93f32d580b
brijesh kumar <brijesh6514@gmail.com>
2025-11-21 17:51:00 +0530

    donee

--- client/app/code/[code]/page.tsx
+++ client/app/code/[code]/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-import { useParams } from "next/navigation";
 import LinkStats from "@/components/LinkStats";
+import { useParams } from "next/navigation";
 
 export default function Page() {
   const params = useParams();
-  return <LinkStats code={params?.code || ""} />;
+
+  // If params.code is an array, pick the first element
+  const code = Array.isArray(params?.code) ? params.code[0] : params?.code || "";
+
+  return <LinkStats code={code} />;
 }
